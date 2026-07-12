e7481c803ac9919fc9233ffb55d1bea6bd2131ea
Maria-agurto <maria.agurtoc@unmsm.edu.pe>
2026-07-12 13:06:00 -0500

    Create participante.h

---
+++ include/participante.h
@@ -0,0 +1,15 @@
+#ifndef PARTICIPANTE_H
+#define PARTICIPANTE_H
+
+#include <string>
+
+struct Participante
+{
+    std::string codigo;
+    std::string nombres;
+    std::string apellidos;
+    std::string carrera;
+    std::string facultad;
+};
+
+#endif
