32ff92a575088005c4696fb6b0a1da932ea8d0ec
Adrian-GPT <adriangp0612@gmail.com>
2026-07-12 02:01:18 -0500

    Add report functions to reportes.h

--- include/reportes.h
+++ include/reportes.h
@@ -1,1 +1,21 @@
+#ifndef REPORTES_H
+#define REPORTES_H
 
+#include "evento.h"
+#include "inscripcion.h"
+#include "participante.h"
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void generarReporteGeneral();
+void eventoMayorAsistencia();
+void eventosDisponibles(); //Lista de eventos con estado Disponible
+void eventosLlenos(); //Lista de eventos con estado Lleno
+void porcentajeOcupacion();
+void ordenarEventos();
+void filtrarEventos();
+
+#endif // REPORTES_H
